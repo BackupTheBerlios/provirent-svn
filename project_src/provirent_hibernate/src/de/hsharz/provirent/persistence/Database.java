7a8dce78894f4afb6114b8b5e30d4aaf49a8bdf6
pschneider <pschneider@3d7621cd-b0e5-0310-b45e-fe56ea984595>
2004-10-21 20:06:54 +0000

    -  Methode für liste von VideoObjects hinzugefügt - Methode für genau ein Videoobject; id übergabe

--- project_src/provirent_hibernate/src/de/hsharz/provirent/persistence/Database.java
+++ project_src/provirent_hibernate/src/de/hsharz/provirent/persistence/Database.java
@@ -45,7 +45,7 @@ import net.sf.hibernate.expression.Expression;
 
 import org.apache.log4j.Logger;
 
-import de.hsharz.provirent.objects.Director;
+import de.hsharz.provirent.objects.*;
 
 
 
@@ -60,45 +60,50 @@ public class Database {
     private static final Logger logger = Logger.getLogger(Database.class);
 
 
+    /**
+     * This method gets all Directors from the database.
+     * searches for firstname or lastname or id
+     * @param filter 
+     * @return List of Director objects, or an empty List
+     */
     public static List getDirectors(final String filter){
         if (logger.isDebugEnabled()) {
             logger.debug("getDirectors() - start. String filter= "+filter);
         }
-
+        //init the returnlist
         List returnlist = new ArrayList();
-        //get new Session and begin Transaction
+
         Session s = null;
         Transaction tx = null;
         try {
+            //get new Session and begin Transaction
             s = HibernateUtil.currentSession();
             tx = s.beginTransaction();
 
             try {
                 
+                //init the criteria
                 Criteria criteria = s.createCriteria(Director.class);
+                //any of the criteria 
                 Disjunction any = Expression.disjunction();
-                
-                
-                
-                if (filter == null || filter.equalsIgnoreCase("")) {
-                	
-                } else {
+
+                //if filter not empty
+                if (filter != null && !filter.equalsIgnoreCase("")) {
                     any.add(Expression.like("lastName", "%"+filter+"%"));
                     any.add(Expression.like("firstName", "%"+filter+"%"));
                     
+                    //maybe we are searching for the id?
                     try {
                         any.add(Expression.eq("directorId", new Integer(Integer.parseInt(filter))));
                     } catch (Exception e) {
                     }
                     
                 }
-
+                //add all criteria
                 criteria.add(any);
+                //get the results
                 returnlist = criteria.list();
 
-                
-                
-                tx.commit();
 
             } catch (Exception e) {
                 if (tx != null) {
@@ -116,7 +121,7 @@ public class Database {
 
         } catch (Exception e) {
             logger.error(
-                    "getDirectors() - Error while trying to beginTransaction",
+                    "getDirectors() - Error while trying to do Transaction",
                     e);
             returnlist = new ArrayList();
         } finally {
@@ -124,15 +129,236 @@ public class Database {
                 // No matter what, close the session
                 HibernateUtil.closeSession();
             } catch (HibernateException e1) {
-                logger.error("getDirectors()", e1);
-
-                // TODO Auto-generated catch block
-
+                logger.error("getDirectors() - Could not Close the Session", e1);
             }
         }
 
         if (logger.isDebugEnabled()) {
             logger.debug("getDirectors() - end");
+        }
+        return returnlist;
+        
+
+    }
+
+    
+    
+    
+    
+    /**
+     * This method gets all VideoFormats from the database.
+     * searches for firstname or lastname or id
+     * @param filter 
+     * @return List of Director objects, or an empty List
+     */
+    public static List getVideoFormat(final String filter){
+        if (logger.isDebugEnabled()) {
+            logger.debug("getVideoFormat() - start. String filter= "+filter);
+        }
+        //init the returnlist
+        List returnlist = new ArrayList();
+
+        Session s = null;
+        Transaction tx = null;
+        try {
+            //get new Session and begin Transaction
+            s = HibernateUtil.currentSession();
+            tx = s.beginTransaction();
+
+            try {
+                
+                //init the criteria
+                Criteria criteria = s.createCriteria(VideoFormat.class);
+                //any of the criteria 
+                Disjunction any = Expression.disjunction();
+
+                //if filter not empty
+                if (filter != null && !filter.equalsIgnoreCase("")) {
+                    any.add(Expression.like("name", "%"+filter+"%"));
+                    any.add(Expression.like("shortname", "%"+filter+"%"));
+                    
+                    //maybe we are searching for the id?
+                    try {
+                        any.add(Expression.eq("videoFormatId", new Integer(Integer.parseInt(filter))));
+                    } catch (Exception e) {
+                    }
+                    
+                }
+                //add all criteria
+                criteria.add(any);
+                //get the results
+                returnlist = criteria.list();
+
+
+            } catch (Exception e) {
+                if (tx != null) {
+                    logger
+                            .error(
+                                    "getVideoFormat() - Something went wrong here; discard all partial changes",
+                                    e);
+
+                    // Something went wrong; discard all partial changes
+                    tx.rollback();
+                }
+
+		                
+            }
+
+        } catch (Exception e) {
+            logger.error(
+                    "getVideoFormat() - Error while trying to do Transaction",
+                    e);
+            returnlist = new ArrayList();
+        } finally {
+            try {
+                // No matter what, close the session
+                HibernateUtil.closeSession();
+            } catch (HibernateException e1) {
+                logger.error("getVideoFormat() - Could not Close the Session", e1);
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("getVideoFormat() - end");
+        }
+        return returnlist;
+        
+
+    }
+
+    
+    public static VideoFormat getSingleVideoFormat(final int id){
+        if (logger.isDebugEnabled()) {
+            logger.debug("getSingleVideoFormat() - start. int filter= "+id);
+        }
+        //init the returnlist
+        VideoFormat returnobject = null;
+
+        Session s = null;
+        Transaction tx = null;
+        try {
+            //get new Session and begin Transaction
+            s = HibernateUtil.currentSession();
+            tx = s.beginTransaction();
+
+            try {
+
+                returnobject = (VideoFormat)s.get(VideoFormat.class, new Integer(id));
+                
+
+
+            } catch (Exception e) {
+                if (tx != null) {
+                    logger
+                            .error(
+                                    "getSingleVideoFormat() - Something went wrong here; discard all partial changes",
+                                    e);
+
+                    // Something went wrong; discard all partial changes
+                    tx.rollback();
+                }
+
+		                
+            }
+
+        } catch (Exception e) {
+            logger.error(
+                    "getSingleVideoFormat() - Error while trying to do Transaction",
+                    e);
+            
+        } finally {
+            try {
+                // No matter what, close the session
+                HibernateUtil.closeSession();
+            } catch (HibernateException e1) {
+                logger.error("getSingleVideoFormat() - Could not Close the Session", e1);
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("getSingleVideoFormat() - end");
+        }
+        return returnobject;
+        
+
+    }
+    
+    
+    /**
+     * This method gets all AudioFormats from the database.
+     * searches for firstname or lastname or id
+     * @param filter 
+     * @return List of Director objects, or an empty List
+     */
+    public static List getAudioFormat(final String filter){
+        if (logger.isDebugEnabled()) {
+            logger.debug("getAudioFormat() - start. String filter= "+filter);
+        }
+        //init the returnlist
+        List returnlist = new ArrayList();
+
+        Session s = null;
+        Transaction tx = null;
+        try {
+            //get new Session and begin Transaction
+            s = HibernateUtil.currentSession();
+            tx = s.beginTransaction();
+
+            try {
+                
+                //init the criteria
+                Criteria criteria = s.createCriteria(AudioFormat.class);
+                //any of the criteria 
+                Disjunction any = Expression.disjunction();
+
+                //if filter not empty
+                if (filter != null && !filter.equalsIgnoreCase("")) {
+                    any.add(Expression.like("name", "%"+filter+"%"));
+                    any.add(Expression.like("shortname", "%"+filter+"%"));
+                    
+                    //maybe we are searching for the id?
+                    try {
+                        any.add(Expression.eq("audioFormatId", new Integer(Integer.parseInt(filter))));
+                    } catch (Exception e) {
+                    }
+                    
+                }
+                //add all criteria
+                criteria.add(any);
+                //get the results
+                returnlist = criteria.list();
+
+
+            } catch (Exception e) {
+                if (tx != null) {
+                    logger
+                            .error(
+                                    "getAudioFormat() - Something went wrong here; discard all partial changes",
+                                    e);
+
+                    // Something went wrong; discard all partial changes
+                    tx.rollback();
+                }
+
+		                
+            }
+
+        } catch (Exception e) {
+            logger.error(
+                    "getAudioFormat() - Error while trying to do Transaction",
+                    e);
+            returnlist = new ArrayList();
+        } finally {
+            try {
+                // No matter what, close the session
+                HibernateUtil.closeSession();
+            } catch (HibernateException e1) {
+                logger.error("getAudioFormat() - Could not Close the Session", e1);
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("getAudioFormat() - end");
         }
         return returnlist;
         
