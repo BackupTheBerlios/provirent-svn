302597e2bbd9d0cf4cdcd4d312ae03c8eacde82c
rgriesch <rgriesch@3d7621cd-b0e5-0310-b45e-fe56ea984595>
2004-11-10 00:32:25 +0000

    Beginn Composite Actors

--- project_src/provirent_hibernate/src/de/hsharz/provirent/managment/gui/CompositeActors.java
+++ project_src/provirent_hibernate/src/de/hsharz/provirent/managment/gui/CompositeActors.java
@@ -6,10 +6,20 @@ import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
 
@@ -17,6 +27,8 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.layout.GridData;
 import com.cloudgarden.resource.SWTResourceManager;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 /**
 * This code was generated using CloudGarden's Jigloo
 * SWT/Swing GUI Builder, which is free for non-commercial
@@ -48,12 +60,25 @@ public class CompositeActors extends AbstractComposite{
     
     
     private final static int MODE_VIEW = 0;
-
     private final static int MODE_ADD = 1;
-    private Text text1;
-    private Group groupActor;
-
     private final static int MODE_EDIT = 2;
+    
+    private Table tableActorsOverview;
+    private TableColumn tableActorsOverview_ColumnID;
+    private TableColumn tableActorsOverview_ColumnFName;
+    private TableColumn tableActorsOverview_ColumnLName;
+    
+    
+    private SashForm sashForm1;
+    private SashForm sashForm2;
+    
+    private Group groupActor;
+    private Group groupActorsOverview;
+    private Group groupActorsDetail;
+    
+    private Text textActorsSearch;
+    
+    private Label labelActorsSearch;
     
     private StatusLineStyledText statusLine;
 
@@ -137,16 +162,181 @@ public class CompositeActors extends AbstractComposite{
             groupActor.setLayoutData(groupActorLData);
             groupActorLayout.makeColumnsEqualWidth = true;
             groupActor.setLayout(groupActorLayout);
-            groupActor.setText("Actor");
+            groupActor.setText(l.getString("actors.group.label"));
             {
-                text1 = new Text(groupActor, SWT.MULTI | SWT.WRAP);
-                text1.setText("text1");
-                GridData text1LData = new GridData();
-                text1.setBackground(SWTResourceManager.getColor(255, 0, 0));
-                text1LData.widthHint = 336;
-                text1LData.heightHint = 173;
-                text1.setLayoutData(text1LData);
+                sashForm2 = new SashForm(groupActor, SWT.NONE);
+                GridData sashForm2LData = new GridData();
+                sashForm2LData.verticalAlignment = GridData.FILL;
+                sashForm2LData.horizontalAlignment = GridData.FILL;
+                sashForm2LData.grabExcessHorizontalSpace = true;
+                sashForm2LData.grabExcessVerticalSpace = true;
+                sashForm2.setLayoutData(sashForm2LData);
+                // Group Actors Overview 
+                {
+                    groupActorsOverview = new Group(sashForm2,SWT.NONE);
+                    GridLayout group1Layout = new GridLayout();
+                    groupActorsOverview.setBounds(0, 0, 781, 487);
+                    groupActorsOverview.setBounds(0, 0, 647, 448);
+                    group1Layout.makeColumnsEqualWidth = true;
+                    GridData group1LData = new GridData();
+                    group1Layout.makeColumnsEqualWidth = true;
+                    groupActorsOverview.setLayout(group1Layout);
+                    groupActorsOverview.setText(l.getString("actors.groupoverview.label"));
+                    {
+                        tableActorsOverview = new Table(
+                            groupActorsOverview,
+                            SWT.SINGLE
+                                | SWT.FULL_SELECTION
+                                | SWT.V_SCROLL | SWT.BORDER);
+                        GridData tableActorsOverviewLData = new GridData();
+                        tableActorsOverview.setHeaderVisible(true);
+                        tableActorsOverview.setLinesVisible(true);
+                        tableActorsOverviewLData.widthHint = GridData.FILL;
+                        tableActorsOverviewLData.heightHint = GridData.FILL;
+                        tableActorsOverviewLData.horizontalSpan = 8;
+                        tableActorsOverviewLData.grabExcessHorizontalSpace = true;
+                        tableActorsOverviewLData.grabExcessVerticalSpace = true;
+                        tableActorsOverview
+                            .setLayoutData(tableActorsOverviewLData);
+                        tableActorsOverview
+                            .addFocusListener(new FocusAdapter() {
+                                public void focusLost(FocusEvent evt) {
+                                    System.out
+                                        .println("tableActorsOverview.focusLost, event="
+                                            + evt);
+                                    //TODO add your code for tableVideoFormat.focusLost
+                                }
+                                public void focusGained(FocusEvent evt) {
+                                    System.out
+                                        .println("tableActorsOverview.focusGained, event="
+                                            + evt);
+                                    //TODO add your code for tableVideoFormat.focusGained
+                                }
+                            });
+                        tableActorsOverview
+                            .addSelectionListener(new SelectionAdapter() {
+                                public void widgetSelected(SelectionEvent evt) {
+                                    if (logger.isDebugEnabled()) {
+                                        logger
+                                            .debug("widgetSelected(SelectionEvent evt = "
+                                                + evt
+                                                + ") - start");
+                                    }
+
+                                    int index = tableActorsOverview
+                                        .getSelectionIndex();
+
+                                    System.out.println("Table select. id: "
+                                        + index
+                                        + " TableItem:"
+                                        + tableActorsOverview.getItem(index)
+                                        + " id: "
+                                        + tableActorsOverview.getItem(index)
+                                            .getText(0));
+
+                                    //es wurde ein Element aus Tabelle ausgewaehlt jetzt muss die
+                                    //Detailansicht aktualisiert werden
+                                    //                               refreshActorsDetail(tableActorsOverview.getItem(index)
+                                    //                                   .getText(0));
+
+                                    if (logger.isDebugEnabled()) {
+                                        logger
+                                            .debug("widgetSelected(SelectionEvent) - end");
+                                    }
+                                }
+                            });
+
+                        {
+                            tableActorsOverview_ColumnID = new TableColumn(
+                                tableActorsOverview,
+                                SWT.CENTER);
+                            tableActorsOverview_ColumnID.setText(l
+                                .getString("actors.groupoverview.columnid"));
+                            tableActorsOverview_ColumnID.setWidth(40);
+                        }
+                        {
+                            tableActorsOverview_ColumnFName = new TableColumn(
+                                tableActorsOverview,
+                                SWT.CENTER);
+                            tableActorsOverview_ColumnFName.setText(l
+                                .getString("actors.groupoverview.columnid"));
+                            tableActorsOverview_ColumnFName.setWidth(80);
+                        }
+                        {
+                            tableActorsOverview_ColumnLName = new TableColumn(
+                                tableActorsOverview,
+                                SWT.CENTER);
+                            tableActorsOverview_ColumnLName.setText(l
+                                .getString("actors.groupoverview.columnid"));
+                            tableActorsOverview_ColumnLName.setWidth(80);
+                        }
+                    }// table Actors Overview
+                    // table Actors Overview
+                    // Search
+                    {
+                        //label für die Suche
+                        labelActorsSearch = new Label(groupActorsOverview, SWT.NONE);
+                        labelActorsSearch.setText(l
+                                .getString("actors.groupoverview.searchlabel")
+                                + ":");
+                        GridData label2LData = new GridData();
+                        label2LData.horizontalSpan = 3;
+                        labelActorsSearch.setLayoutData(label2LData);
+
+                        //das Suchfeld
+                        textActorsSearch = new Text(groupActorsOverview, SWT.BORDER);
+                        GridData text2LData = new GridData();
+                        textActorsSearch.addFocusListener(new FocusAdapter() {
+                            public void focusLost(FocusEvent evt) {
+                                if (logger.isDebugEnabled()) {
+                                    logger.debug("focusLost(FocusEvent evt = " + evt + " "
+                                            + textActorsSearch.getText() + ") - start");
+                                }
+
+                   //             refreshVideoFormatTable(textVideoFormatSearch.getText());
+
+                                if (logger.isDebugEnabled()) {
+                                    logger.debug("focusLost(FocusEvent) - end");
+                                }
+                            }
+                        });
+                        textActorsSearch.addListener(SWT.DefaultSelection, new Listener() {
+                            public void handleEvent(Event e) {
+                                if (logger.isDebugEnabled()) {
+                                    logger.debug("handleEvent(Event e = " + e + " "
+                                            + textActorsSearch.getText() + ") - start");
+                                }
+
+                //                refreshVideoFormatTable(textActorsSearch.getText());
+
+                                if (logger.isDebugEnabled()) {
+                                    logger.debug("handleEvent(Event) - end");
+                                }
+                            }
+                        });
+                        text2LData.horizontalAlignment = GridData.FILL;
+                        text2LData.horizontalSpan = 5;
+                        text2LData.grabExcessHorizontalSpace = true;
+                        textActorsSearch.setLayoutData(text2LData);
+                    }// Search
+                    
+                
+                }
+                // Group Actors Detail
+                {
+                    groupActorsDetail = new Group(sashForm2, SWT.NONE);
+                    GridLayout group2Layout = new GridLayout();
+                    groupActorsDetail.setBounds(0, 0, 781, 487);
+                    groupActorsDetail.setBounds(0, 0, 647, 448);
+                    group2Layout.makeColumnsEqualWidth = true;
+                    GridData group2LData = new GridData();
+                    group2Layout.makeColumnsEqualWidth = true;
+                    groupActorsDetail.setLayout(group2Layout);
+                    groupActorsDetail.setText(l.getString("actors.groupdetail.label"));   
+                }
             }
+            // init the rest of the layout
+            initActorsGroup();
         }
 
         this.layout();
@@ -158,6 +348,7 @@ public class CompositeActors extends AbstractComposite{
         }
 	}
 
-	
+	private void initActorsGroup()  {	    
+	}
 
 }
